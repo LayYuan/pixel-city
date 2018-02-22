46c000ee9f06d9674de15e5a7bf5dd98acf688a7
Yuan25 <chuahlayyuan@gmail.com>
2018-02-22 20:25:53 +0800

    Initial Commit

---
+++ pixel-city/View/DroppablePin.swift
@@ -0,0 +1,22 @@
+//
+//  DroppablePin.swift
+//  pixel-city
+//
+//  Created by LayYuan on 21/02/2018.
+//  Copyright © 2018 justCodeEnterprise. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DroppablePin: NSObject, MKAnnotation {
+    
+    dynamic var coordinate: CLLocationCoordinate2D
+    var identifier: String
+    
+    init(coordinate: CLLocationCoordinate2D, identifier: String){
+        self.coordinate = coordinate
+        self.identifier = identifier
+        super.init()
+    }
+}
